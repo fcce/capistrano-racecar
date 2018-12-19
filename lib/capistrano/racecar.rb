97647d6ff5163a856e080a8574cf0210c01676dc
Feng Ce <kalelfc@gmail.com>
2018-12-19 15:01:37 +0800

    init

---
+++ lib/capistrano/racecar.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'capistrano/bundler'
+require 'capistrano/plugin'
+module Capistrano
+  class Racecar < Capistrano::Plugin
+    CAP_FILES = %w[
+      stop
+      start
+      restart
+    ]
+
+    def define_tasks
+      Capistrano::Racecar::CAP_FILES.each do |cap_file|
+        eval File.open(File.expand_path("../tasks/#{cap_file}.cap", __FILE__), 'r').read
+      end
+    end
+
+    def set_defaults
+      set_if_empty :racecar_pid_prefix, 'racecar'
+      set_if_empty :racecar_pid_path, 'pids'
+    end
+
+    # Deploy hooks registration
+    def register_hooks
+      # deploy:published
+      after 'deploy:published', 'racecar:restart'
+    end
+    SSHKit.config.command_map[:racecar] = "bundle exec racecar"
+  end
+end
+
